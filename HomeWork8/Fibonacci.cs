a6578b8d89d9f20afeaa1e62255272c86b87f0dc
belan <smakki.92@gmail.com>
2024-11-15 11:58:48 +0300

    Добавьте файлы проекта.

---
+++ HomeWork8/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿
+namespace HomeWork8
+{
+    internal class Fibonacci
+    {
+
+        public static int Reccursion(int n)
+        {
+            return n <= 1 ? n : Reccursion(n - 1) + Reccursion(n - 2);
+        }
+
+        public static int Loop(int n)
+        {
+            var a = 0;
+            var b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                (a, b) = (b, a + b);  
+            }
+            return a;
+        }
+    }
+}
---
+++ HomeWork8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace HomeWork8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+
+            //sw.Restart();
+            //for (int i = 0; i <= 20; i++)
+            //{
+            //    Console.WriteLine("The {0}st term of the Fibonacci sequence is {1}.", i, Fibonacci.Reccursion(i));
+            //}
+            //sw.Stop();
+            //Console.WriteLine("the time spent on finding the Fibonacci sequence of 20 elements using the recursion method was {0} ms.", sw.ElapsedTicks);
+            //Console.WriteLine();
+
+
+            //sw.Restart();
+            //for (int i = 0; i <= 20; i++)
+            //{
+            //    Console.WriteLine("The {0}st term of the Fibonacci sequence is {1}.", i, Fibonacci.Loop(i));
+            //}
+            //sw.Stop();
+            //Console.WriteLine("the time spent searching for a Fibonacci sequence of 20 elements in a loop was {0} ms.",sw.ElapsedTicks);
+            //Console.WriteLine();
+
+
+            int[] nums = { 5, 10, 20 };
+
+            foreach (var n in nums)
+            {
+                sw.Restart();
+                var value = Fibonacci.Reccursion(n);
+                sw.Stop();
+                Console.WriteLine("The {0}st term of the Fibonacci sequence is {1}.", n, value);
+                Console.WriteLine("the time spent on finding {0} member of the Fibonacci sequence using the recursion method was {1} ticks.", n, sw.ElapsedTicks);
+                Console.WriteLine();
+            }
+
+            foreach (var n in nums)
+            {
+                sw.Restart();
+                var value = Fibonacci.Loop(n);
+                sw.Stop();
+                Console.WriteLine("The {0}st term of the Fibonacci sequence is {1}.", n, value);
+                Console.WriteLine("the time spent on finding {0} member of the Fibonacci sequence using the loop was {1} ticks.", n, sw.ElapsedTicks);
+                Console.WriteLine();
+            }
+        }
+    }
+}
